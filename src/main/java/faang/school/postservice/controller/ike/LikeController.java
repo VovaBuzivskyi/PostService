d3554402a6c738a398943ea23a5450220d9a44fb
VovaBuzivskyi <vov4ikvseti@gmail.com>
2025-03-16 21:46:55 +0700

    added: controller methods getLikedPostUsers, getLikedCommentUsers

--- src/main/java/faang/school/postservice/controller/ike/LikeController.java
+++ src/main/java/faang/school/postservice/controller/ike/LikeController.java
@@ -1,15 +1,19 @@
 package faang.school.postservice.controller.ike;
 
 import faang.school.postservice.dto.like.LikeDto;
+import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.service.like.LikeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,4 +41,15 @@ public class LikeController {
     public void removeLikeFromComment(@PathVariable Long likeId, @RequestBody @Valid LikeDto likeDto) {
         likeService.removeLikeFromComment(likeId, likeDto);
     }
+
+    @GetMapping("/posts/{postId}")
+    public List<UserDto> getLikedPostUsers(@PathVariable Long postId) {
+        return likeService.getLikedPostUsers(postId);
+    }
+
+    @GetMapping("/comments/{commentId}")
+    public void getLikedCommentUsers(@PathVariable Long commentId) {
+        return likeService.getLikedCommentUsers(commentId);
+    }
 }
+
